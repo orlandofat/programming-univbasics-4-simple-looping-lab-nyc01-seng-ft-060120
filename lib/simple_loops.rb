d474afbc1ef5bf6e8690a51f7b7a310013802f84
Orlando Challenger <orlandofat@gmail.com>
2020-05-08 21:31:16 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,32 @@
 # Write your methods here
+def loop_message_five_times(message)
+ count = 0
+ while count < 5 do
+   puts message 
+ count += 1
+ end
+end
+
+def loop_message_n_times(message, num)
+count = 0
+ while count < num do
+   puts message 
+ count += 1
+ end
+end
+
+def output_array(array)
+count = 0
+ while count < array.length do
+   puts array
+ count += 1
+ end
+end
+
+def return_string_array(array)
+count = 0
+ while count < array.length do
+   puts array
+ count += 1
+ end
+end
